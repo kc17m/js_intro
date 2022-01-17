e4ce6c2eb4e2c3e7498177863e4ffb07e0f9a123
SuperCoder <supercode@AirvonSuperCode.fritz.box>
2022-01-17 14:35:05 +0100

    1

---
+++ assets/js/main.js
@@ -0,0 +1,176 @@
+//lev1_4
+
+window.alert("Hallo Welt");
+
+let alter = prompt("Git mir bitte dein Alter:");
+console.log(alter);
+
+window.prompt("Bitte geben Sie Ihren Namen ein", "cancel");
+
+window.confirm("Stimmen Sie zu?");
+
+let b = 5;
+let a = b * 5 - 3;
+alert(a);
+
+
+let mann = "Robert Wadlow ist der größte Mann der Welt: ";
+let groesse = "2,72m";
+alert(mann + groesse);
+
+
+
+//lev1_1 
+
+// let a = 12;
+// let b = a * 3;
+// console.log(b);
+// document.write(b);
+// console.log(document.URL);
+// console.log("Hello World");
+
+// let name = "Anton";
+// console.log(name);
+
+// let age = 28;
+// console.log(age);
+
+// let job = "Trainer";
+// console.log(job);
+
+// let married = true;
+// console.log(married);
+
+// console.log(name + " ist " + age + " und ist verheiratet: " + " married");
+
+
+
+//lev1_5 
+
+// let carName = "BMW";
+// console.log(carName);
+
+// let x = 150;
+// console.log(x);
+
+// let y = 50;
+// console.log(y);
+
+// let z = x + y;
+// console.log(z);
+
+// let firstName, lastName, age;
+
+// firstName = "John";
+// lastName = "Doe";
+// age = 35;
+
+// console.log(firstName + " " + lastName + " " + age);
+
+// lev1_6
+// let z = 34;
+// z = 67;
+//console.log(z);
+
+/*let x = 34;
+x = 67;
+console.log(x);
+*/
+
+// const y = 34;
+// y = 67;
+// console.log(y) //const nicht überschreiben
+
+
+
+
+
+//lev1_7
+
+// let helloWorld = "Hello World"
+// const pi = 3.14;
+// const burjDubai = "828m";
+// let fullName = "Jan Schmidt";
+// const eifeltower = "324m";
+// let camelCase = "camelCase";
+// let kursstatus = true;
+
+//lev1_12
+
+// let score = 5 + 5 * 10;
+// console.log("Ergebnis: " + score);
+
+// let score1 = (5 + 5) * 10;
+// console.log("Ergebnis: " + score1);
+
+// let score3 = 0;
+// score3 = score3 + 10;
+// console.log("Ergebnis: " + score3);
+
+// score3 += 10;
+// console.log("Ergebnis: " + score3);
+
+// let zahl = 1;
+// console.log(zahl)
+// zahl = zahl + 1;
+// console.log(zahl)
+// zahl += 1;
+// console.log(zahl);
+
+// zahl++;
+// console.log("increment: " + zahl);
+
+// zahl--;
+// console.log("decrement: " + zahl);
+
+
+// lev1_13
+
+// let addition_operator = 1 + 1;
+// console.log("addition: " + addition_operator);
+
+// let subtraktion_operator = 2 - 1;
+// console.log("subtraktion: " + subtraktion_operator);
+
+// let multiplication_operatior = 2 * 2;
+// console.log("multiplikation: " + multiplication_operatior);
+
+// let division_operator = 4 / 2;
+// console.log("division: " + division_operator);
+
+// let modulus_operator = 14 % 5;
+// console.log("modulus: " + modulus_operator);
+
+//lev1_1
+
+document.write("<h1>Hello World</h1>");
+document.write("<p>Have a nice day!</p>");
+
+let myText = "Hello again";
+document.write(myText + "<br>");
+
+let name = "Monika"; let nachname = "Dauer";
+
+document.write("Name: " + name + "<br>Nachname: " + nachname + "<br>");
+
+
+//lev1_11
+
+let x = "Hello ";
+let y = "World<br>";
+
+let z = x + y;
+
+document.write(z);
+
+let n = x + " " + y;
+x += y;
+
+
+document.write(x);
+
+
+
+
+
+
